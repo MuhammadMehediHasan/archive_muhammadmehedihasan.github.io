0999c462ca50b71abc9260efae150561c5b19014
MuhammadMehediHasan <mehedisohan1111@gmail.com>
2021-01-21 22:42:01 +0600

    s

---
+++ downloader/dist/js/app.js
@@ -0,0 +1,14 @@
+var convertBtn = document.querySelector(".convert-button");
+var urlInput = document.querySelector(".url-input");
+
+convertBtn.addEventListener("click", () => {
+  sendURL(urlInput.value);
+});
+
+function sendURL(URL) {
+  //   fetch(`http://localhost:4000/download?url=${URL}`, {method: "GET"})
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+
+  window.location.href = `https://ytdldownloader.herokuapp.com//download?url=${URL}`;
+}
